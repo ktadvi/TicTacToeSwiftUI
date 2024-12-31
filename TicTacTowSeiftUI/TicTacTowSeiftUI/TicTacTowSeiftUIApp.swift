e19a36c5f6f5029dea1bfea0ca307bea334e5b62
ktadvi <ktadvi5696@gmail.com>
2024-12-31 04:26:31 -0500

    Initial project setup
    - Created Xcode project with default settings
    - Set up folder structure (Model, View, ViewModel, etc.)
    - Added .gitignore and README.md

---
+++ TicTacTowSeiftUI/TicTacTowSeiftUI/TicTacTowSeiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacTowSeiftUIApp.swift
+//  TicTacTowSeiftUI
+//
+//  Created by Khushbu Tadvi on 2024-12-31.
+//
+
+import SwiftUI
+
+@main
+struct TicTacTowSeiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TicTacTowSeiftUI/TicTacTowSeiftUITests/TicTacTowSeiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacTowSeiftUITests.swift
+//  TicTacTowSeiftUITests
+//
+//  Created by Khushbu Tadvi on 2024-12-31.
+//
+
+import Testing
+@testable import TicTacTowSeiftUI
+
+struct TicTacTowSeiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
